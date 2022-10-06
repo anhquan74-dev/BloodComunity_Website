ce19ac7ce88059eb1635495c5ae99abaafa42374
HoangKhiem3k <ledohoangkhiem3k@gmail.com>
2022-10-06 19:18:15 +0700

    api: update user

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -134,10 +134,25 @@ let handleDeteleUser = async (req, res) => {
   }
 };
 let handleUpdateUser = async (req, res) => {
-  let data = req.body;
-  let message = await userService.updateUserService(data);
-  return res.status(200).json(message);
+  try {
+    if (!req.body.id) {
+      res.send({
+        statusCode: 422,
+        message: "Missing input user id!",
+      });
+    } else {
+      let userUpdated = await userService.updateUserService(req.body);
+      res.status(200).json(userUpdated);
+    }
+  } catch (e) {
+    console.log(e);
+    res.send({
+      statusCode: 500,
+      message: "Error from server!",
+    });
+  }
 };
+
 module.exports = {
   handleRegister,
   handleLogin,
