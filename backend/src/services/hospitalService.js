827aa68bb8d979c4766a0eda6ccc9632a33635f0
HoangKhiem3k <ledohoangkhiem3k@gmail.com>
2022-10-08 15:56:04 +0700

    api: get-all-schedule-by-date

--- backend/src/services/hospitalService.js
+++ backend/src/services/hospitalService.js
@@ -7,12 +7,10 @@ let bulkCreateScheduleService = async (data) => {
   try {
     let scheduleCreate = {};
     if (!data.arrSchedule || !data.hospitalId || !data.formatedDate) {
-      console.log("data", data);
       scheduleCreate.statusCode = 422;
       scheduleCreate.message = "Missing schedules data";
     } else {
       let schedule = data.arrSchedule;
-      console.log("arr schedule", schedule);
 
       // check existing in database
       let existing = await db.Schedule.findAll({
@@ -20,6 +18,7 @@ let bulkCreateScheduleService = async (data) => {
         attributes: ["timeType", "date", "hospitalId", "maxNumber"],
         raw: true,
       });
+
       // convert date
       // if (existing && existing.length > 0) {
       //   existing = existing.map((item) => {
@@ -44,48 +43,51 @@ let bulkCreateScheduleService = async (data) => {
     console.log(e);
   }
 };
-let getScheduleByDate = (doctorId, date) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      if (!doctorId || !date) {
-        resolve({
-          errCode: 1,
-          errMessage: "Missing parameters",
-        });
-      } else {
-        let dataSchedule = await db.Schedule.findAll({
-          where: {
-            doctorId: doctorId,
-            date: date,
+let getScheduleByDateService = async (hospitalId, date) => {
+  try {
+    let scheduleData = {};
+    if (!hospitalId || !date) {
+      scheduleData.statusCode = 422;
+      scheduleData.message = "Missing require parameters!";
+    } else {
+      let data = await db.Schedule.findAll({
+        where: {
+          hospitalId,
+          date,
+        },
+        include: [
+          {
+            model: db.Allcode,
+            as: "timeTypeData",
+            attributes: ["valueVi", "valueEn"],
           },
-          include: [
-            {
-              model: db.Allcode,
-              as: "timeTypeData",
-              attributes: ["valueVi", "valueEn"],
-            },
-            {
-              model: db.User,
-              as: "doctorData",
-              attributes: ["firstName", "lastName"],
-            },
-          ],
-          raw: false,
-          nest: true,
-        });
-        if (!dataSchedule) dataSchedule = [];
-        resolve({
-          errCode: 0,
-          data: dataSchedule,
-        });
+          {
+            model: db.User,
+            as: "hospitalData",
+            // attributes: ["firstName", "lastName"],
+            attributes: ["hospitalName"],
+          },
+        ],
+        raw: false,
+        nest: true,
+      });
+      if (!data || data.length === 0) {
+        scheduleData.statusCode = 404;
+        scheduleData.message = "Not found!";
+        data = [];
+      } else {
+        scheduleData.statusCode = 200;
+        scheduleData.message = "Get schedule data successfully!";
+        scheduleData.content = data;
       }
-    } catch (e) {
-      reject(e);
+      return scheduleData;
     }
-  });
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 module.exports = {
   bulkCreateScheduleService,
-  getScheduleByDate,
+  getScheduleByDateService,
 };
